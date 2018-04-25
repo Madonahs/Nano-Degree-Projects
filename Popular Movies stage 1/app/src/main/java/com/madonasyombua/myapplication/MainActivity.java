8d2d8bebf704bce812f7544e54bfd1706a7adec9
Madonahs <syombuamadona@gmail.com>
2018-04-25 11:37:55 -0500

    start project

--- Popular Movies stage 1/app/src/main/java/com/madonasyombua/myapplication/MainActivity.java
+++ Popular Movies stage 1/app/src/main/java/com/madonasyombua/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.madonasyombua.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author madon
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
