a1b1c314ec4f628d6063faf6078d8e8113c7a948
Madonahs <syombuamadona@gmail.com>
2018-04-25 11:53:07 -0500

    sunshine app

--- Sunshine App/app/src/main/java/com/madonasyombua/myapplication/ScrollingActivity.java
+++ Sunshine App/app/src/main/java/com/madonasyombua/myapplication/ScrollingActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * @author madon
+ */
 public class ScrollingActivity extends AppCompatActivity {
 
     @Override
