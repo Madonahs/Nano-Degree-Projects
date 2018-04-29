e9f722f40f85cd4118656bac5e0d4e18e11aa635
Madonahs <syombuamadona@gmail.com>
2018-04-29 12:57:33 -0500

    review

--- Sunshine App/app/src/main/java/com/madonasyombua/myapplication/ScrollingActivity.java
+++ Sunshine App/app/src/main/java/com/madonasyombua/myapplication/ScrollingActivity.java
@@ -18,10 +18,10 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//will replace this.
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        FloatingActionButton fab =findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
