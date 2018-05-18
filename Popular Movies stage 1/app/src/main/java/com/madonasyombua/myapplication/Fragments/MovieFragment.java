d0f4270867e35f8c441bfb61c30a6ff62fb587d6
Madonahs <syombuamadona@gmail.com>
2018-05-17 21:51:12 -0500

    movie model

--- Popular Movies stage 1/app/src/main/java/com/madonasyombua/myapplication/Fragments/MovieFragment.java
+++ Popular Movies stage 1/app/src/main/java/com/madonasyombua/myapplication/Fragments/MovieFragment.java
@@ -4,6 +4,7 @@ package com.madonasyombua.myapplication.Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,8 +26,6 @@ public class MovieFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
@@ -57,13 +56,13 @@ public class MovieFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            String mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie, container, false);
