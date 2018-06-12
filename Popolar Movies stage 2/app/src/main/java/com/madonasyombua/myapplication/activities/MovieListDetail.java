6f29c881ad0664eff48ffa7d836636b0e731e296
Madonahs <syombuamadona@gmail.com>
2018-06-11 21:16:36 -0500

    on progress

--- Popolar Movies stage 2/app/src/main/java/com/madonasyombua/myapplication/activities/MovieListDetail.java
+++ Popolar Movies stage 2/app/src/main/java/com/madonasyombua/myapplication/activities/MovieListDetail.java
@@ -76,53 +76,6 @@ public class MovieListDetail extends AppCompatActivity implements DBUpdateListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list_detail);
         ButterKnife.bind(this);
-/*
-        Intent intent = getIntent();
-        Movie movie = intent.getParcelableExtra(getString(R.string.movie_parcel));
-
-        float vote = (float) (movie.getMovieVote() / 2);
-        AppCompatRatingBar ratingBar = findViewById(R.id.rating);
-        ratingBar.setIsIndicator(true);
-        ratingBar.setStepSize(0.1f);
-        ratingBar.setRating(vote);
-
-        originalTitle.setText(movie.getTitle());
-        releaseDateTitle.setText("Release Date");
-
-        Picasso.with(this)
-                .load(movie.getMoviePosterPath())
-                .resize(187,285)
-                .placeholder(R.drawable.loading)
-                .error(R.drawable.alert_circle_outline)
-                .into(imageViewPoster);
-
-        String overView = movie.getMovieOverview();
-
-        if(overView == null){
-            tvOverview.setTypeface(null, Typeface.BOLD_ITALIC);
-            overView = getResources().getString(R.string.error);
-        }
-
-        tvOverview.setText(overView);
-        voteAverage.setText(movie.getVoteAverage());
-
-        String releaseDate = movie.getMovieRelease();
-        if(releaseDate != null){
-
-            try{
-                releaseDate = MovieHelper.LocalizedDate(this, releaseDate,movie.getDate());
-            }catch (ParseException pe){
-                Log.e(TAG,String.valueOf(R.string.release_date_error),pe);
-            }
-        }else {
-
-            releaseDate = getResources().getString(R.string.no_date_found);
-
-        }
-
-        tvReleaseDate.setText(releaseDate);
-
-    }*/
 
 
     }
