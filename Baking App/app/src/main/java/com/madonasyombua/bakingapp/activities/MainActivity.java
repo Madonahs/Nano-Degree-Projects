34108b191c2b0636af873f5984b4ef6dc6141f84
Madonahs <syombuamadona@gmail.com>
2018-06-17 13:28:20 -0500

    on progress

--- Baking App/app/src/main/java/com/madonasyombua/bakingapp/activities/MainActivity.java
+++ Baking App/app/src/main/java/com/madonasyombua/bakingapp/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.madonasyombua.bakingapp;
+package com.madonasyombua.bakingapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.madonasyombua.bakingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
