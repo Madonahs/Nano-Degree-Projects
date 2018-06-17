e50c6e1464cac1c0f6fdb00a6a77df874f38b6dc
Madonahs <syombuamadona@gmail.com>
2018-06-17 13:37:16 -0500

    on progress

--- Baking App/app/src/main/java/com/madonasyombua/bakingapp/activities/MainActivity.java
+++ Baking App/app/src/main/java/com/madonasyombua/bakingapp/activities/MainActivity.java
@@ -2,10 +2,15 @@ package com.madonasyombua.bakingapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.madonasyombua.bakingapp.R;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
