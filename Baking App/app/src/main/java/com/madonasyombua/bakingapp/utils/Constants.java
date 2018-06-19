34d3ad11c724639312bb87f7c210cc5acf590be0
Madonahs <syombuamadona@gmail.com>
2018-06-19 09:12:38 -0500

    on progress'

--- Baking App/app/src/main/java/com/madonasyombua/bakingapp/utils/Constants.java
+++ Baking App/app/src/main/java/com/madonasyombua/bakingapp/utils/Constants.java
@@ -1,4 +1,7 @@
 package com.madonasyombua.bakingapp.utils;
 
 public class Constants {
+
+    /* the Recipe URL is offered by Udacity*/
+    public static final String RECIPE_URL ="https://d17h27t6h515a5.cloudfront.net/";
 }
--- Baking App/app/src/main/java/com/madonasyombua/bakingapp/widget/AppWidget.java
+++ Baking App/app/src/main/java/com/madonasyombua/bakingapp/widget/AppWidget.java
@@ -1,9 +1,11 @@
-package com.madonasyombua.bakingapp;
+package com.madonasyombua.bakingapp.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
+
+import com.madonasyombua.bakingapp.R;
 
 /**
  * Implementation of App Widget functionality.
