6954c7ad75855bcd0ecf0332707c6806a56ee09b
Madonahs <syombuamadona@gmail.com>
2018-07-02 19:19:40 -0500

    accepted

--- Baking App/app/src/main/java/com/madonasyombua/bakingapp/model/Ingredients.java
+++ Baking App/app/src/main/java/com/madonasyombua/bakingapp/model/Ingredients.java
@@ -1,4 +1,109 @@
 package com.madonasyombua.bakingapp.model;
 
-public class Ingredients {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@JsonIgnoreProperties (ignoreUnknown = true)
+public class Ingredients implements Parcelable {
+
+    @JsonProperty("quantity")
+    private Double quantity;
+    @JsonProperty("measure")
+    private String measure;
+    @JsonProperty("ingredient")
+    private String ingredient;
+
+
+    //constructor
+    public Ingredients(Double quantity, String measure, String ingredient) {
+        this.quantity = quantity;
+        this.measure = measure;
+        this.ingredient = ingredient;
+    }
+
+    /**
+     * get Quantity method
+     * @return quantity
+     */
+    public Double getQuantity() {
+        return quantity;
+    }
+
+
+    /**
+     * get Measure method
+     * @return measure
+     */
+    public String getMeasure() {
+        return measure;
+    }
+
+    /**
+     * get ingredients method
+     * @return ingredient
+     */
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    /**
+     * Describe the kinds of special objects contained in this Parcelable
+     * instance's marshaled representation. For example, if the object will
+     * include a file descriptor in the output of {@link #writeToParcel(Parcel, int)},
+     * the return value of this method must include the
+     * {@link #CONTENTS_FILE_DESCRIPTOR} bit.
+     *
+     * @return a bitmask indicating the set of special object types marshaled
+     * by this Parcelable object instance.
+     */
+    public int describeContents() {
+        return 0;
+    }
+    public final static Parcelable.Creator<Ingredients> CREATOR = new Creator<Ingredients>() {
+
+
+        public Ingredients createFromParcel(Parcel in) {
+            return new Ingredients(in);
+        }
+
+        public Ingredients[] newArray(int size) {
+            return (new Ingredients[size]);
+        }
+
+    };
+
+    /**
+     * Flatten this object in to a Parcel.
+     *
+     * @param dest The Parcel in which the object should be written.
+     * @param flags Additional flags about how the object should be written.
+     * May be 0 or {@link #PARCELABLE_WRITE_RETURN_VALUE}.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeDouble(this.quantity);
+        dest.writeString(this.measure);
+        dest.writeString(this.ingredient);
+    }
+
+    /**
+     *  An unusual feature of Parcel is the ability to read and write active
+     * objects.  For these objects the actual contents of the object is not
+     * written, rather a special token referencing the object is written.  When
+     * reading the object back from the Parcel, you do not get a new instance of
+     * the object, but rather a handle that operates on the exact same object that
+     * was originally written.  There are two forms of active objects available.</p>
+     * @param in read
+     */
+    protected Ingredients(Parcel in) {
+        this.quantity = in.readDouble();
+        this.measure = in.readString();
+        this.ingredient = in.readString();
+    }
+
 }
