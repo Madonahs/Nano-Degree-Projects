37f8bff8e56feb6af88498143db2a0b49edc9774
Madonahs <syombuamadona@gmail.com>
2018-07-11 21:38:00 -0500

    working on it

---
+++ Make Your Material App/README.adoc
@@ -0,0 +1,3 @@
+= XYZ Reader Starter Code
+
+This repository stores the starter code for the XYZ Reader project in Udacity's Android Nanodegree.
---
+++ Make Your Material App/XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -0,0 +1,23 @@
+package com.example.xyzreader.remote;
+
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Config {
+    public static final URL BASE_URL;
+    private static String TAG = Config.class.toString();
+
+    static {
+        URL url = null;
+        try {
+            url = new URL("https://go.udacity.com/xyz-reader-json" );
+        } catch (MalformedURLException ignored) {
+            // TODO: throw a real error
+            Log.e(TAG, "Please check your internet connection.");
+        }
+
+        BASE_URL = url;
+    }
+}
