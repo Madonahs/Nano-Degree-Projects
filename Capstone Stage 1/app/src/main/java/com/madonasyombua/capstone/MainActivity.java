5b4cb3b20a66ddc27dec5d71fb9bf82fac72b642
Madonahs <syombuamadona@gmail.com>
2018-07-29 07:14:00 -0500

    added design

--- Capstone Stage 1/app/src/main/java/com/madonasyombua/capstone/MainActivity.java
+++ Capstone Stage 1/app/src/main/java/com/madonasyombua/capstone/MainActivity.java
@@ -2,7 +2,7 @@ package com.madonasyombua.capstone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//code in the straight talk repo
 public class MainActivity extends AppCompatActivity {
 
     @Override
